d2009c8fdcd01beafad553b2b47ee6ebcca84633
Mike <mwroberts89@gmail.com>
2022-09-10 09:27:38 -0400

    installed react-dom-router...actually pretty simple for basic stuff. Videos in news section taking time to load though

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import Header from './components/Header';
+import HeaderNav from './components/HeaderNav';
 import Releases from './components/Releases';
 import Footer from './components/Footer';
 import News from './components/News';
 import History from './components/History';
 import Contact from './components/Contact';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return <>
-    <Header />
-    <Releases />
-    <News />
-    <History />
-    <Contact />
-    <Footer />
+  <Router>
+    <HeaderNav />
+
+  <Routes>
+    <Route path="/" element={<Releases />} />
+    <Route path="/news" element={<News />} />
+    <Route path="/history" element={<History />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+  
+  <Footer />
+  </Router>
   </>;
 }
 
